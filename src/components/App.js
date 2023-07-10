4cd3f7debf573e5910dda5c89b8a096c1408215f
anitha-anii <anithachowdary355@gmail.com>
2023-07-10 07:37:47 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>You've been on this page for {seconds} seconds.</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
